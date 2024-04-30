bda6603302d6407c78e5b52eb88a16e446108434
macedonsky777 <31594772+macedonsky777@users.noreply.github.com>
2024-04-30 23:56:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Hello world from new image version 2!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
